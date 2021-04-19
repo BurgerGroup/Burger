43a51a7f25a19ea31f18bff766aab85fa2fa2e21
yuxiao <2286229152@qq.com>
2021-04-19 17:13:27 +0800

    add option:console/file

--- burger/base/Log.cc
+++ burger/base/Log.cc
@@ -4,6 +4,9 @@
 
 using namespace burger;
 
+std::atomic<bool> Logger::writeToConsole_{true};
+std::atomic<bool> Logger::writeToFile_{true};
+
 // suppot for coid
 class co_formatter_flag : public spdlog::custom_flag_formatter {
 public:
@@ -27,6 +30,10 @@ bool Logger::init(const std::string& filePath,
                 const std::string& loggerName, 
                 spdlog::level::level_enum level) {
     if(isInited_) return true;
+    if(!writeToFile_ && !writeToConsole_) {
+        std::cout << "Initialized AN EMPTY Logger!" << std::endl;
+        return true;
+    }
     try {
         // check log path and try to create log directory
         // 这里需要文件操作吗?
@@ -43,11 +50,18 @@ bool Logger::init(const std::string& filePath,
         spdlog::flush_every(std::chrono::seconds(3));  // todo 这里能影响道logger吗
         // spdlog::flush_on(spdlog::level::warn);
 
-        auto console_sink = std::make_shared<spdlog::sinks::stdout_color_sink_mt>();
-        auto file_sink = std::make_shared<spdlog::sinks::rotating_file_sink_mt> (filePath, 1024*1024*5, 5, false);
         // auto file_sink = std::make_shared<spdlog::sinks::basic_file_sink_mt>(filePath, true);
         // Async : https://github.com/gabime/spdlog/wiki/6.-Asynchronous-logging
-        std::vector<spdlog::sink_ptr> sinks{console_sink, file_sink};
+        std::vector<spdlog::sink_ptr> sinks;
+        if(writeToConsole_) {
+            auto console_sink = std::make_shared<spdlog::sinks::stdout_color_sink_mt>();
+            sinks.push_back(std::move(console_sink));
+        }
+
+        if(writeToFile_) {
+            auto file_sink = std::make_shared<spdlog::sinks::rotating_file_sink_mt> (filePath, 1024*1024*5, 5, false);
+            sinks.push_back(std::move(file_sink));
+        }
         // std::vector<spdlog::sink_ptr> sinks{file_sink};   // 暂时先不要输出到显示屏
         std::shared_ptr<spdlog::logger> logger = std::make_shared<spdlog::logger>(loggerName,  sinks.begin(), sinks.end());
         logger->set_level(level);    // 需要单独设置logger的level      
