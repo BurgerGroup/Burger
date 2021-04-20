7300202fb7ff1512c8019cb04574ce71e7daa746
Shiyu Yi <www.769440972@qq.com>
2021-04-20 18:45:38 +0800

    Update Processor.h

--- burger/net/Processor.h
+++ burger/net/Processor.h
@@ -13,35 +13,35 @@ class Epoll;
 class Scheduler;
 class Processor : boost::noncopyable {
 public:
-	using ptr = std::shared_ptr<Processor>;
+    using ptr = std::shared_ptr<Processor>;
 
-	Processor(Scheduler* scheduler);
-	virtual ~Processor() {}
+    Processor(Scheduler* scheduler);
+    virtual ~Processor() {}
 
-	virtual void run();
-	void stop();
-	bool stoped() { return stop_; }
-	size_t getLoad() { return load_; }
-	Scheduler* getScheduler() { return scheduler_; }
-	void addTask(Coroutine::ptr co);
-	void addTask(const Coroutine::Callback& cb, std::string name = "");
-	// void updateEvent(int fd, int events, Coroutine::ptr coroutine = nullptr);
-	// void removeEvent(int fd);
+    virtual void run();
+    void stop();
+    bool stoped() { return stop_; }
+    size_t getLoad() { return load_; }
+    Scheduler* getScheduler() { return scheduler_; }
+    void addTask(Coroutine::ptr co);
+    void addTask(const Coroutine::Callback& cb, std::string name = "");
+    // void updateEvent(int fd, int events, Coroutine::ptr coroutine = nullptr);
+    // void removeEvent(int fd);
 
-	static Processor* GetProcesserOfThisThread();
+    static Processor* GetProcesserOfThisThread();
 
 private:
-	void wakeupEpollCo();
-	ssize_t comsumeWakeEvent();
+    void wakeupEpollCo();
+    ssize_t comsumeWakeEvent();
 
-	bool stop_ = false;
-	size_t load_ = 0;
+    bool stop_ = false;
+    size_t load_ = 0;
     std::mutex mutex_;
-	Scheduler* scheduler_;
+    Scheduler* scheduler_;
     std::unique_ptr<Epoll> epoll_;
 
-	int eventFd_;
-	std::list<Coroutine::ptr> coList_;
+    int eventFd_;
+    std::list<Coroutine::ptr> coList_;
 };
 
 
